0cf25f6e923fc6b7445900b81e680760900b4d48
Esko Nuutila <Esko.Nuutila@aalto.fi>
2015-06-01 21:54:28 +0300

    .

--- tools/test_statistics.py
+++ tools/test_statistics.py
@@ -50,15 +50,22 @@ def msg(txt):
         o.write(txt+'\n')
         print(txt)
 
+refs=[]
+
+def see(file):
+    global refs
+    refs.append(file)
+    return ' See [{}]'.format(len(refs))
+
 def check_counts(part1, part2, total, indent, dp1, dp2, dtot, difference_file=None):
     diff1=total.minus(part1.union(part2))
     if len(diff1) > 0:
-        msg(format('='+'='*indent+'> len({}):{} > {} = len({}):{} + len({}):{}!'.format(dtot, len(total), len(part1) + len(part2), dp1, len(part1), dp2, len(part2))+(' See '+difference_file if difference_file else '')))
+        msg(format('='+'='*indent+'> len({}):{} > {} = len({}):{} + len({}):{}!'.format(dtot, len(total), len(part1) + len(part2), dp1, len(part1), dp2, len(part2))+see(difference_file if difference_file else '')))
         if difference_file: diff1.write(difference_file)
         return False
     diff2=part1.union(part2).minus(total)
     if len(diff2) > 0:
-        msg(format('='+'='*indent+'> len({}):{} < {} = len({}):{} + len({}):{}!'.format(dtot, len(total), len(part1) + len(part2), dp1, len(part1), dp2, len(part2))+(' See '+difference_file if difference_file else '')))
+        msg(format('='+'='*indent+'> len({}):{} < {} = len({}):{} + len({}):{}!'.format(dtot, len(total), len(part1) + len(part2), dp1, len(part1), dp2, len(part2))+see(difference_file if difference_file else '')))
         if difference_file: diff2.write(difference_file)
         return False
 
@@ -118,68 +125,71 @@ write_non_empty(query_runnable_implemented_ran_not_compared,query_runnable_imple
 write_non_empty(failed,failed_file)
 
 msg('\nTest suite, collection, and type statistics:')
-msg('* {} tests. See {}'.format(len(all), input_file))
-msg('* {} test suites. See {}'.format(len(suite), suite_file))
-msg('* {} test collections. See {}'.format(len(suite_collection), suite_collection_file))
-msg('* {} types. See {}'.format(len(types), type_file))
-msg('* {} collections with two types. See {}'.format(len(suite_collection_double_type), suite_collection_double_type_file))
+msg('* {} tests.{}'.format(len(all), see(input_file)))
+msg('* {} test suites.{}'.format(len(suite), see(suite_file)))
+msg('* {} test collections.{}'.format(len(suite_collection), see(suite_collection_file)))
+msg('* {} types.{}'.format(len(types), see(type_file)))
+msg('* {} collections with two types.{}'.format(len(suite_collection_double_type), see(suite_collection_double_type_file)))
 
 msg('\nTest execution statistics:')
 msg('\n* {:>4} tests in total'.format(len(all)))
 if len(not_implemented) > 0:
-    msg('--> {} test not executed, because features not implemented in INSTANS. See {}'.format(len(not_implemented), not_implemented_file))
+    msg('--> {} test not executed, because features not implemented in INSTANS.{}'.format(len(not_implemented), see(not_implemented_file)))
 check_counts(implemented, not_implemented, all, 6, 'tests implemented', 'tests not implemented', 'all tests', implemented_undefined_file)
 
 msg('\nSyntax test execution statistics:')
 msg('\n*{:>4} syntax tests.'.format(len(syntax)))
 msg('      * {} negative syntax tests.'.format(len(negative_syntax)))
 if len(negative_syntax_parsed) > 0:
-    msg('------------> {:>3} negative syntax tests unexpectedly parsed! See {}'.format(len(negative_syntax_parsed), negative_syntax_parsed_file))
+    msg('------------> {:>3} negative syntax tests unexpectedly parsed!{}'.format(len(negative_syntax_parsed), see(negative_syntax_parsed_file)))
 msg('            * {:>3} negative syntax tests did not parse.'.format(len(negative_syntax_not_parsed)))
 check_counts(negative_syntax_parsed, negative_syntax_not_parsed, negative_syntax, 12, 'negative syntax parsed ', 'negative syntax not parsed ', 'negative syntax', negative_syntax_parsed_undefined_file)
 
 msg('      * {} positive syntax tests.'.format(len(positive_syntax)))
 if len(positive_syntax_not_parsed) > 0:
-    msg('-------------> {} positive syntax tests did not parse! See {}'.format(len(positive_syntax_not_parsed), positive_syntax_not_parsed_file))
+    msg('-------------> {} positive syntax tests did not parse!{}'.format(len(positive_syntax_not_parsed), see(positive_syntax_not_parsed_file)))
 
 msg('            * {} positive syntax tests parsed.'.format(len(positive_syntax_parsed)))
 check_counts(positive_syntax_not_parsed, positive_syntax_parsed, positive_syntax, 12, 'positive syntax not parsed', 'positive syntax parsed', 'positive syntax', positive_syntax_parsed_undefined_file)
 
 if len(positive_syntax_parsed_not_translated) > 0:
-    msg('------------------> {:>3} positive syntax tests parsed, but did not translate! See {}'.format(len(positive_syntax_parsed_not_translated), positive_syntax_parsed_not_translated_file))
+    msg('------------------> {:>3} positive syntax tests parsed, but did not translate!{}'.format(len(positive_syntax_parsed_not_translated), see(positive_syntax_parsed_not_translated_file)))
 msg('                  * {:>3} positive syntax tests parsed and translated.'.format(len(positive_syntax_parsed_translated)))
 check_counts(positive_syntax_parsed_translated, positive_syntax_parsed_not_translated, positive_syntax_parsed, 18, 'positive syntax parsed translated', 'positive syntax parsed not translated', 'positive syntax parsed', positive_syntax_parsed_translated_undefined_file)
 
 if len(positive_syntax_parsed_translated_not_initialized) > 0:
-    msg('------------------------> {:>3} positive syntax tests parsed and translated, but did not initialize! See {}'.format(len(positive_syntax_parsed_translated_not_initialized), positive_syntax_parsed_translated_not_initialized_file))
+    msg('------------------------> {:>3} positive syntax tests parsed and translated, but did not initialize!{}'.format(len(positive_syntax_parsed_translated_not_initialized), see(positive_syntax_parsed_translated_not_initialized_file)))
 msg('                        * {:>3} positive syntax tests parsed, translated and initialized.'.format(len(positive_syntax_parsed_translated_initialized)))
 check_counts(positive_syntax_parsed_translated_initialized, positive_syntax_parsed_translated_not_initialized, positive_syntax_parsed_translated, 24, 'positive syntax parsed translated initialized', 'positive syntax parsed translated not initialized', 'positive syntax parsed translated', positive_syntax_parsed_translated_initialized_undefined_file)
 
 msg('\nQuery evalution test execution statistics:')
 msg('\n*{:>4} query evaluation tests.'.format(len(query)))
-msg('------> {:>3} query evaluation tests not runnable (not parsed, translated, or initialized)! See {}'.format(len(query_not_runnable), query_not_runnable_file))
+msg('------> {:>3} query evaluation tests not runnable (not parsed, translated, or initialized)!{}'.format(len(query_not_runnable), see(query_not_runnable_file)))
 msg('      * {:>3} query evaluation tests runnable (parsed, translated, initialized).'.format(len(query_runnable)))
 check_counts(query_runnable, query_not_runnable, query, 6, 'query runnable', 'query not runnable', 'query', query_runnable_undefined_file)
 
 if len(query_runnable_not_implemented) > 0:
-    msg('------------> {:>3} runnable query evaluation tests with non-implemented operations in INSTANS! See {}'.format(len(query_runnable_not_implemented), query_runnable_not_implemented_file))
+    msg('------------> {:>3} runnable query evaluation tests with non-implemented operations in INSTANS!{}'.format(len(query_runnable_not_implemented), see(query_runnable_not_implemented_file)))
 msg('            * {:>3} query_runnable evaluation tests fully implemented in INSTANS.'.format(len(query_runnable_implemented)))
 check_counts(query_runnable_implemented, query_runnable_not_implemented, query_runnable, 6, 'query runnable implemented', 'query runnable not implemented', 'query runnable', query_runnable_implemented_undefined_file)
 
 if len(query_runnable_implemented_not_ran) > 0:
-    msg('------------------> {:>3} query runnable evaluation tests fully implemented in INSTANS, but dit not run! See {}'.format(len(query_runnable_implemented_not_ran), query_runnable_implemented_not_ran_file))
+    msg('------------------> {:>3} query runnable evaluation tests fully implemented in INSTANS, but dit not run!{}'.format(len(query_runnable_implemented_not_ran), see(query_runnable_implemented_not_ran_file)))
 msg('                  * {:>3} query runnable evaluation tests implemented and ran.'.format(len(query_runnable_implemented_ran)))
 check_counts(query_runnable_implemented_ran, query_runnable_implemented_not_ran, query_runnable_implemented, 18, 'query runnable implemented ran', 'query runnable implemented not ran', 'query runnable implemented', query_runnable_implemented_ran_undefined_file)
 
 if len(query_runnable_implemented_ran_not_compared) > 0:
-    msg('------------------------> {:>3} query runnable evaluation tests fully implemented in INSTANS, ran, but dit not compare! See {}'.format(len(query_runnable_implemented_ran_not_compared), query_runnable_implemented_ran_not_compared_file))
+    msg('------------------------> {:>3} query runnable evaluation tests fully implemented in INSTANS, ran, but dit not compare!{}'.format(len(query_runnable_implemented_ran_not_compared), see(query_runnable_implemented_ran_not_compared_file)))
 msg('                        * {:>3} query runnable evaluation tests implemented ran and compared.'.format(len(query_runnable_implemented_ran_compared)))
 check_counts(query_runnable_implemented_ran_compared, query_runnable_implemented_ran_not_compared, query_runnable_implemented_ran, 24, 'query runnable implemented ran compared', 'query runnable implemented ran not compared', 'query runnable implemented ran', query_runnable_implemented_ran_compared_undefined_file)
 
 msg('\nSummary')
 msg('\n* {:>4} tests in total'.format(len(all)))
 if len(failed) > 0:
-    msg('-------> {} tests failed. See {}'.format(len(failed), failed_file))
+    msg('-------> {} tests failed.{}'.format(len(failed), see(failed_file)))
 msg('       * {:>3} tests succeeded.'.format(len(not_failed)))
 check_counts(failed, not_failed, all, 6, 'tests failed', 'tests not failed', 'all tests', failed_undefined_file)
+msg('\nReferences:')
+for i in range(len(refs)):
+    msg('[{}] {}'.format(i+1, refs[i]))
 sys.exit(0)
