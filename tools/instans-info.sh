bab629bbf05c6280ecb93503280b895ace3f5a08
Esko Nuutila <Esko.Nuutila@aalto.fi>
2015-06-18 22:54:59 +0300

    .

---
+++ tools/instans-info.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+INSTANS_HOME=$1
+INSTANS=$2
+INSTANS_BIN=$3
+STATISTICS=$4
+cd $INSTANS_HOME
+{
+echo =============================
+echo INSTANS_HOME=$INSTANS_HOME
+echo INSTANS=$INSTANS
+echo INSTANS_BIN=$INSTANS_BIN
+echo =============================
+echo ls -l $INSTANS_BIN:
+echo
+ls -l $INSTANS_BIN
+echo =============================
+echo $INSTANS --version:
+echo
+$INSTANS --version
+echo =============================
+echo git branch:
+echo
+git branch
+echo =============================
+echo git status:
+echo
+git status
+echo =============================
+echo git log:
+echo
+git log
+} > $STATISTICS/instans.info
+
