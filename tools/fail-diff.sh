abf395303e2f1efd66c2af209ec9f5d6c68e3e8c
Esko Nuutila <Esko.Nuutila@aalto.fi>
2015-08-09 20:32:23 +0300

    Changed reporting to list not-failed.csv. Added fail-diff.sh to tools

--- tools/fail-diff.sh
+++ tools/fail-diff.sh
@@ -22,14 +22,15 @@ cmp_one_pair() {
 	diff $FILE1 $FILE2 > $TMP
 	egrep '^<' $TMP | sed '/^< /s///' > $TMP1 
 	egrep '^>' $TMP | sed '/^> /s///' > $TMP2
-	echo $(wc $TMP1) "tests in $FILE1 but not in $FILE2"
+	echo `wc -l $TMP1 | awk '{print $1}'` "tests in NEW/$1 but not in OLD/$1"
 	if [ "$SEE_DIFFERENCES" != "" ]; then
 	    cat $TMP1
 	fi
-	echo $(wc $TMP2) "tests in $FILE2 but not in $FILE1"
+	echo `wc -l $TMP2 | awk '{print $1}'` "tests in OLD/$1 but not in NEW/$1"
 	if [ "$SEE_DIFFERENCES" != "" ]; then
 	    cat $TMP2
 	fi
+	rm -f $TMP $TMP1 $TMP2
     fi
 }
 
